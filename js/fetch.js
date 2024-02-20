daef9341e58de99576facb3dc1f70105671ad591
Aayam Adhikari <thisisleo7@gmail.com>
2024-02-20 15:59:42 +0545

    complete version 2

---
+++ js/fetch.js
@@ -0,0 +1,31 @@
+const boardMemberContainer = document.querySelector(".board-members");
+
+
+const fetchMembers = () => {
+  fetch("../data/members.json")
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+
+      data.forEach(person => {
+        const member = document.createElement("div");
+        member.classList.add("member");
+        member.innerHTML = `
+                <div class="card">
+                          <img src="${person.url}" alt="Jane Smith">
+                          <div class="member-info">
+                            <h3>${person.name}</h3>
+                            <p>${person.position}</p>
+                          </div>
+                        </div>
+                `
+
+        boardMemberContainer.append(member)
+        
+      })
+
+    })
+
+}
+
+fetchMembers();
